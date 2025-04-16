30ad9485455c51ddddc44c3f02c3f9324dda7dd9
Emanuela Wenning Welinski <73607390+manuwenning@users.noreply.github.com>
2025-04-16 14:43:45 -0300

    Fix database

--- CadastroFastAPI/database.py
+++ CadastroFastAPI/database.py
@@ -1,12 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Substitua pelos seus dados reais
-DATABASE_URL = "postgresql://postgres:LZZpPLET6iCt5ZFZ@db.ymarbpmondxgajrfnccr.supabase.co:5432/postgres"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
